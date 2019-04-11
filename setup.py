3aa28974b656b5c399ebf6ab45ae8eceb3aa96bb
Valentin Niess <niess@in2p3.fr>
2019-04-11 21:11:04 +0200

    Add the geoid data to the dist and update PyPi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from grand_pkg import setup_package
 # The package version
 MAJOR = 0
 MINOR = 1
-MICRO = 2
+MICRO = 3
 
 
 # Extra package meta data can be added here. For a full list of available
@@ -25,7 +25,10 @@ if __name__ == "__main__":
         # Vanilla setuptools.setup arguments can be added below,
         # e.g. `entry_points` for executables or `data_files`
         install_requires = (
-            "grand-libs>=0.1.4",
-            "astropy>=3.0.0")
+            "grand-libs>=0.1.5",
+            "astropy>=3.0.0"),
+        package_data = {
+            "grand_tools": ["data/egm96.png"]
+        }
     )
 
